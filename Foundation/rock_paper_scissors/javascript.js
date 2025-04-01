5aaae2a2a1c1fe85044077bcfbdc9af334036b7c
Pua Yong Hao <puayonghao@outlook.com>
2025-04-01 21:23:33 +0800

    create html and js file for rock paper scissors game

---
+++ Foundation/rock_paper_scissors/javascript.js
@@ -0,0 +1,71 @@
+function getComputerChoice() {
+  const value = Math.random();
+  if (value <= 0.33) return "rock";
+  else if (value <= 0.66) return "paper";
+  else return "scissors";
+}
+
+function getHumanChoice() {
+  const choice = parseInt(
+    prompt("Please enter your choice:\n0: rock\n1: paper\n2: scissors")
+  );
+  if (choice === 0) return "rock";
+  else if (choice === 1) return "paper";
+  else if (choice === 2) return "scissors";
+  else return "Invalid";
+}
+
+function toTitleCase(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function decideIfHumanWin(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) return -1;
+  if (
+    (humanChoice === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice === "Paper" && computerChoice === "Rock") ||
+    (humanChoice === "Scissors" && computerChoice === "Paper")
+  )
+    return 1;
+  else return 0;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  const titleCase_humanChoice = toTitleCase(humanChoice);
+  const titleCase_computerChoice = toTitleCase(computerChoice);
+  const didHumanWin = decideIfHumanWin(
+    titleCase_humanChoice,
+    titleCase_computerChoice
+  );
+  if (didHumanWin === 1) {
+    humanScore += 1;
+    console.log(
+      `You win! ${titleCase_humanChoice} beats ${titleCase_computerChoice}`
+    );
+  } else if (didHumanWin === 0) {
+    computerScore += 1;
+    console.log(
+      `You lose! ${titleCase_computerChoice} beats ${titleCase_humanChoice}`
+    );
+  } else
+    console.log(
+      `Draw! ${titleCase_computerChoice} and ${titleCase_humanChoice}`
+    );
+}
+
+function playGame() {
+  for (let index = 0; index < 5; index++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+  console.log(
+    `Final score: ${humanScore} (Human) vs ${computerScore} (Computer)`
+  );
+}
+
+playGame();
