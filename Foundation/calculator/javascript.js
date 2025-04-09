25324711c7efcdd214345349522628ec3a70b9b3
Pua Yong Hao <puayonghao@outlook.com>
2025-04-10 00:05:48 +0800

    feat: add all required functions for a calculator. chore: better function name and reduce the flag number.

--- Foundation/calculator/javascript.js
+++ Foundation/calculator/javascript.js
@@ -7,142 +7,117 @@ const equalButton = document.querySelector(".equal-button");
 const valueText = document.querySelector(".value-text");
 
 let operator;
-let operand1 = 0;
-let operand2 = 0;
+let operand1;
+let operand2;
+let startNewValue = true;
 
-function appendDisplayText(inputDigit) {
-  if (valueText.textContent.length >= 10) return;
-  if (valueText.textContent === "0") valueText.textContent = inputDigit;
-  else valueText.textContent += inputDigit;
-  // if (operatorCount >= 2) {
-  //   const str =
-  //     valueText.textContent.substring(0, valueText.textContent.length - 1) +
-  //     inputDigit;
-  //   valueText.textContent = str;
-  //   return;
-  // }
-  // if (valueText.textContent === "0" || isResult) {
-  //   valueText.textContent = inputDigit;
-  //   isResult = false;
-  // } else valueText.textContent += inputDigit;
-}
+const MAX_CHAR_ALLOWED = 9;
 
-function numButtonFunction() {
-  appendDisplayText(this.textContent);
-}
-
-function operatorButtonFunction() {
-  operator = this.textContent;
-  if (!operand1) operand1 = parseInt(valueText.textContent);
-}
-
-function clearButtonFunction() {
+function clear() {
   valueText.textContent = "0";
   operator = undefined;
-  operand1 = 0;
-  operand2 = 0;
+  operand1 = undefined;
+  operand2 = undefined;
+  startNewValue = true;
 }
 
-function signButtonFunction() {
+function toggleSign() {
   if (valueText.textContent === "0") return;
-  if (valueText.textContent.charAt(0) !== "-")
-    valueText.textContent = "-" + valueText.textContent;
-  else valueText.textContent = valueText.textContent.slice(1);
+  valueText.textContent = valueText.textContent.startsWith("-")
+    ? valueText.textContent.slice(1)
+    : `-${valueText.textContent}`;
 }
 
-function dotButtonFunction() {
-  if (valueText.textContent.indexOf(".") === -1) valueText.textContent += ".";
+function addDecimal() {
+  if (!valueText.textContent.includes(".")) updateDisplay(".");
+}
+
+function updateDisplay(buttonText) {
+  if (startNewValue || valueText.textContent === "0") {
+    valueText.textContent = buttonText;
+    startNewValue = false;
+  } else if (valueText.textContent.length < MAX_CHAR_ALLOWED) {
+    valueText.textContent += buttonText;
+  }
+}
+
+function setOperator() {
+  if (startNewValue || !operator) {
+    operand1 = !valueText.textContent.includes(".")
+      ? parseInt(valueText.textContent)
+      : parseFloat(valueText.textContent);
+  } else {
+    calculate();
+  }
+  operator = this.textContent;
+  startNewValue = true;
 }
 
 function add() {
-  operand1 = operand1 + operand2;
+  return operand1 + operand2;
 }
 
 function subtract() {
-  operand1 = operand1 - operand2;
+  return operand1 - operand2;
 }
 
 function multiply() {
-  operand1 = operand1 * operand2;
+  return operand1 * operand2;
 }
 
 function divide() {
   if (operand2 === 0) {
-    valueText.textContent = "Infinity";
-    return;
+    return "Infinity";
   }
-  operand1 = parseFloat((operand1 / operand2).toFixed(7));
+  return parseFloat((operand1 / operand2).toFixed(7));
 }
 
 function modulus() {
-  operand1 = operand1 % operand2;
+  return operand1 % operand2;
 }
 
-function equalButtonFunction() {
+function calculate() {
+  operand2 = !valueText.textContent.includes(".")
+    ? parseInt(valueText.textContent)
+    : parseFloat(valueText.textContent);
   switch (operator) {
     case "+":
-      add();
+      operand1 = add();
       break;
     case "-":
-      subtract();
+      operand1 = subtract();
       break;
     case "*":
-      multiply();
+      operand1 = multiply();
       break;
     case "/":
-      divide();
+      operand1 = divide();
       break;
     case "%":
-      modulus();
+      operand1 = modulus();
       break;
     default:
       break;
   }
+  valueText.textContent = operand1;
 }
 
-clearButton.addEventListener("click", clearButtonFunction);
-signButton.addEventListener("click", signButtonFunction);
-dotButton.addEventListener("click", dotButtonFunction);
-equalButton.addEventListener("click", equalButtonFunction);
+function showResult() {
+  if (operator) calculate();
+  operator = undefined;
+  operand2 = undefined;
+  startNewValue = true;
+}
+
+clearButton.addEventListener("click", clear);
+signButton.addEventListener("click", toggleSign);
+dotButton.addEventListener("click", addDecimal);
+equalButton.addEventListener("click", showResult);
 numButtonList.forEach((numButton) => {
-  numButton.addEventListener("click", numButtonFunction);
+  numButton.addEventListener("click", () =>
+    updateDisplay(numButton.textContent)
+  );
 });
 operatorButtonList.forEach((operatorButton) => {
-  operatorButton.addEventListener("click", operatorButtonFunction);
+  operatorButton.addEventListener("click", setOperator);
 });
-
-// let isResult = false;
-// let operatorCount = 0;
-
-// function changeDisplayText(text) {
-//   valueText.textContent = text;
-// }
-
-// function showResult() {
-//   const str = valueText.textContent;
-//   let startIndex = 0;
-//   const operandStack = [];
-//   const operatorStack = [];
-//   for (let i = 0; i < str.length; i++) {
-//     if (!isNumber(str.charAt(i))) {
-//       operandStack.push(str.substring(startIndex, i));
-//       operatorStack.push(str.charAt(i));
-//       startIndex = i + 1;
-//     }
-//   }
-//   if (startIndex !== str.length)
-//     operandStack.push(str.substring(startIndex, str.length));
-//   if (operandStack.length <= operatorStack.length) return;
-//   let total = parseInt(operandStack.shift());
-//   while (!!operandStack.length && !!operatorStack.length) {
-//     const operand2 = parseInt(operandStack.shift());
-//     const operator = operatorStack.shift();
-//     if (operand2 === 0 && operator === "/") {
-//       valueText.textContent = "Infinity";
-//       return;
-//     }
-//     total = operate(operator, total, operand2);
-//   }
-//   changeDisplayText(total);
-//   isResult = true;
-// }
